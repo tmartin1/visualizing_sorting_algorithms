480a417bccfaf03cfb4612f13aaf8dafcba4a177
Tim Martin <timothy.m.martin@gmail.com>
2015-01-09 23:20:11 -0800

    Updated d3 animation and display.

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,19 +1,32 @@
+/* To do:
+ *	 - complete merge sort
+ *	 - complete selection sort
+ *	 - complete shell sort
+ *	 - complete heap sort
+ *	 - refator sort methods such that display functions and compare
+ *		 functions are processed by the same external function that takes
+ *		 the array and a callback as parameters.
+ * 	 - Add table to index.html to show the comparrison of itterations
+ * 		 for each of the different sorting algorithms.
+ */
+
 // Populate dropdown list with sorting algorithms
 for (var x in sorts) {
 	var newOption = $('<option>');
-	newOption.text(x);
+	newOption.val(x);
+	newOption.text(sorts[x].name);
 	$('#sortOptions').append(newOption);
 }
 
 // Build display area
-var height = 300;
-var width = 600;
-var lineWidth = 3;
+var rHeight = 300;
+var rWidth = 600;
+var lineWidth = 10;
 var unsorted = [];
 
 d3.select('#display').append('svg')
-  .attr('width', width)
-  .attr('height', height)
+  .attr('width', rWidth)
+  .attr('height', rHeight)
 	.append('g');
 
 var populate = function(data) {
@@ -22,7 +35,7 @@ var populate = function(data) {
 		.enter().append('rect')
 		.attr('class', 'rect')
     .attr('x', function(d,i) { return i*lineWidth; })
-    .attr('y', 0)
+    .attr('y', function(d,i) { return rHeight-d; })
     .attr('height', function(d,i) { return d; })
     .attr('width', lineWidth);
 };
@@ -31,23 +44,38 @@ var update = function(data) {
   var items = d3.selectAll('rect')
     .data(data)
 		.transition()
-			.ease('elastic')
-			.style('color', 'red')
-	    .attr('x', function(d,i) { return i*lineWidth; })
-	    .attr('height', function(d,i) { return d; });
+			.ease('linear')
+			.each(function() {
+				d3.selectAll('rect').transition()
+			    .attr('x', function(d,i) {
+			    	// if ($(this).attr('x') !== i*lineWidth) d3.select(this).style('fill', 'red');
+			    	return i*lineWidth; 
+			    })
+		    	.attr('y', function(d,i) { return rHeight-d; })
+			    .attr('height', function(d,i) {
+			    	// if ($(this).attr('height') !== d) d3.select(this).style('fill', 'blue');
+			    	return d;
+			    });
+		  });
 };
+
+var enterTransition
 
 var randomize = function() {
 	$('#itterations').text('0');
 	unsorted = [];
-	for (var i=0; i<width/lineWidth; i++) {
-		unsorted.push(Math.floor(Math.random()*height));
+	for (var i=0; i<rWidth/lineWidth; i++) {
+		unsorted.push(Math.floor(Math.random()*rHeight));
 	}
 	update(unsorted);
+	for (var x in sorts) {
+		sorts[x].itterations = 0;
+	}
 };
 randomize();
 
 var startSort = function() {
-	sorts[$('#sortOptions').val()](unsorted);
+	sorts[$('#sortOptions').val()].display(unsorted);
 };
+
 populate(unsorted);
--- javascripts/sorts.js
+++ javascripts/sorts.js
@@ -1,52 +1,231 @@
+// Helper function to process sorts for the display.
+var displaySolve = function(array, callback) {
+	(function timedLoop(i) {
+	  setTimeout(function() {
+	  	//
+	  }, 1)
+	})(0)
+};
+
+// Helper function to process sorts for comparrisons.
+var simpleSolve = function(array, callback) {
+	//
+};
+
+// Object to hold all sort functions and properties.
 var sorts = {};
 
-// Each sort function is it's own object which takes an array (which it
-// will sort), and a function 'update' which it will call to update the d3
-// upon each itteration.
-sorts.shiftSort = function(array) {
-	arr = array.slice();
-	var itterations = 0;
-	(function timedLoop(i) {
-		setTimeout(function() {
-			if (arr[i] < arr[i+1]) {
+sorts.insertionSort = {
+	name: 'Insertion Sort',
+	itterations: 0,
+	display: function(array) {
+		var arr = array.slice();
+		(function timedLoop(i) {
+			setTimeout(function() {
+				if (arr[i] > arr[i+1]) {
+		      var temp = arr[i];
+		      arr[i] = arr[i+1];
+		      arr[i+1] = temp;
+		      i -= 2;
+		    	update(arr);
+		    }
+		    $('#itterations').text(++sorts['insertionSort'].itterations);
+		    if (i < arr.length) timedLoop(++i);
+			}, 1)
+		})(0);
+	  return arr;
+	},
+	compare: function(array) {
+		var arr = array.slice();
+	  for (var i=0; i < arr.length; i++) {
+	    if (arr[i] > arr[i+1]) {
 	      var temp = arr[i];
 	      arr[i] = arr[i+1];
 	      arr[i+1] = temp;
 	      i -= 2;
-	    	update(arr);
 	    }
-	    itterations++;
-	    $('#itterations').text(itterations);
-	    if (i < arr.length) timedLoop(++i);
-		}, 1)
-	})(0);
-  return arr;
+	    this.itterations++;
+	  }
+		return this.itterations;
+	}
 };
 
-sorts.bubbleSort = function(array, itterations, noChange) {
-	arr = array.slice();
-	itterations = itterations || 0;
-  // base case
-  if (noChange) {
-  	update(arr);
-  	return arr;
-  }
-  // recursive case
-  var noChange = true;
-	(function innerloop(i) {
-		setTimeout(function() {
-	  	if (arr[i] < arr[i+1]) {
+sorts.bubbleSort = {
+	name: 'Bubble Sort',
+	itterations: 0,
+	display: function(array, noChange) {
+		arr = array.slice();
+		itterations = itterations || 0;
+	  if (noChange) return;
+	  var noChange = true;
+		(function timedLoop(i) {
+			setTimeout(function() {
+		  	if (arr[i] > arr[i+1]) {
+		  		var temp = arr[i];
+		  		arr[i] = arr[i+1];
+		  		arr[i+1] = temp;
+		  		noChange = false;
+		  		update(arr);
+		  	}
+		  	$('#itterations').text(++sorts['bubbleSort'].itterations);
+		    if (i < arr.length) timedLoop(++i);
+		    else sorts['bubbleSort'].display(arr, noChange);
+			}, 1)
+		})(0)
+	},
+	compare: function(array, noChange, itterations) {
+		var arr = array.slice();
+	  if (noChange) return this.itterations;
+	  var noChange = true;
+	  for (var i=0; i < arr.length; i++) {
+	  	if (arr[i] > arr[i+1]) {
 	  		var temp = arr[i];
 	  		arr[i] = arr[i+1];
 	  		arr[i+1] = temp;
 	  		noChange = false;
-	  		update(arr);
 	  	}
-	  	itterations++;
-	  	$('#itterations').text(itterations);
-	    if (i < arr.length) innerloop(++i);
-	    else sorts.bubbleSort(arr, itterations, noChange);
-		}, 1)
-	})(0);
+	  	this.itterations++;
+	  }
+	  return sorts['bubbleSort'].compare(arr, noChange);
+	}
 };
+
+// unfinished, not working
+sorts.quickSort = {
+	name: 'Quick Sort',
+	itterations: 0,
+	display: function(array, fractal) {
+		var arr = array.slice();
+		var fractal = fractal || arr.slice();
+		if (arr.length === 0) return [];
+		var left = [];
+		var right = [];
+		var pivot = arr[0];
+
+		(function timedLoop(i) {
+		  setTimeout(function() {
+		  	// var temp = arr.shift();
+		  	if(arr[i] > pivot) {
+		  		left.push(arr[i]);
+		  	} else {
+		  		right.push(arr[i]);
+		  	}
+    		// console.log('left',left);
+    		// console.log('right',right);
+    		var temp = fractal.shift();
+    		console.log(left.concat(right, fractal).length)
+
+		  	// update(left.concat(right, fractal));
+		  	
+		  	$('#itterations').text(++sorts['quickSort'].itterations);
+		    if (i < arr.length) {
+		    	timedLoop(++i);
+		    } else {
+		    	// fractal.unshift(temp);
+		    }
+		  }, 1)
+		    	sorts['quickSort'].display(left, fractal).concat(pivot, sorts['quickSort'].display(right, fractal));
+		  update(arr);
+		})(0)
+	},
+	compare: function(array) {
+		if (array.length === 0) return [];
+		var arr = array.slice();
+		var left = [], right = [], pivot = arr[0];
+    for (var i = 1; i < arr.length; i++) {
+    	if(arr[i] < pivot) left.push(arr[i]);
+    	else right.push(arr[i]);
+    }
+    return this.compare(left).concat(pivot, this.compare(right));
+	}
+};
+
+// unfinished, not working
+sorts.mergeSort = {
+	name: 'Merge Sort',
+	itterations: 0,
+	display: function(array) {
+		var arr = array.slice();
+		itterations = 0;
+		//
+		(function timedLoop(i) {
+		  setTimeout(function() {
+		  	//
+		  }, 1)
+		})(0)
+	},
+	compare: function(array) {
+		var arr = array.slice();
+		itterations = 0;
+		//
+	}
+};
+
+// unfinished, not working
+sorts.selectionSort = {
+	name: 'Selection Sort',
+	itterations: 0,
+	display: function(array) {
+		var arr = array.slice();
+		itterations = 0;
+		//
+		(function timedLoop(i) {
+		  setTimeout(function() {
+		  	//
+		  }, 1)
+		})(0)
+	},
+	compare: function(array) {
+		var arr = array.slice();
+		itterations = 0;
+		//
+	}
+};
+
+// unfinished, not working
+sorts.shellSort = {
+	name: 'Shell Sort',
+	itterations: 0,
+	display: function(array) {
+		var arr = array.slice();
+		itterations = 0;
+		//
+		(function timedLoop(i) {
+		  setTimeout(function() {
+		  	//
+		  }, 1)
+		})(0)
+	},
+	compare: function(array) {
+		var arr = array.slice();
+		itterations = 0;
+		//
+	}
+};
+
+// unfinished, not working
+sorts.heapSort = {
+	name: 'Heap Sort',
+	itterations: 0,
+	display: function(array) {
+		var arr = array.slice();
+		itterations = 0;
+		//
+		(function timedLoop(i) {
+		  setTimeout(function() {
+		  	//
+		  }, 1)
+		})(0)
+	},
+	compare: function(array) {
+		var arr = array.slice();
+		itterations = 0;
+		//
+	}
+};
+
+
+
+
+
 
